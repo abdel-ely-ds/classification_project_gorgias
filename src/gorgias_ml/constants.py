38b05eab5e70f5032fe1f5304570114b9d39f975
abdel-ely-ds <abdel.ely.ds@gmail.com>
2023-08-05 21:09:00 +0200

    fix imports

--- src/gorgias_ml/constants.py
+++ src/gorgias_ml/constants.py
@@ -2,3 +2,13 @@ EMBEDDINGS_COL_NAME = "email_sentence_embeddings"
 FEATURES = "features"
 TARGET = "contact_reason"
 PREDICTION = "prediction"
+ACCOUNT_ID = "account_id"
+
+ARTIFACTS_DIRECTORY = "artifacts"
+PIPELINES_DIRECTORY = "pipelines"
+MODELS_DIRECTORY = "models"
+PREDICTIONS_DIRECTORY = "predictions"
+
+PIPELINE_NAME = "pipeline"
+MODEL_NAME = "model"
+PREDICTION_NAME = "prediction"
