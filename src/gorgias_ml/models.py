37dd67e75a669569fa0955e69a0e36c32d88fc09
abdel-ely-ds <abdel.ely.ds@gmail.com>
2023-08-05 18:49:31 +0200

    fix model

--- src/gorgias_ml/models.py
+++ src/gorgias_ml/models.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import scipy as sp
 import swifter
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.base import BaseEstimator
 
 import gorgias_ml.constants as cst
 
 
-class TicketMessageSimilarityBasedClassifier(BaseEstimator, TransformerMixin):
+class TicketMessageSimilarityBasedClassifier(BaseEstimator):
     """Cosine similarity based classifier"""
 
     def __init__(
@@ -15,43 +15,51 @@ class TicketMessageSimilarityBasedClassifier(BaseEstimator, TransformerMixin):
         features_col_name: str = cst.FEATURES,
         prediction_col_name: str = cst.PREDICTION,
     ):
+        self._classes = None
+        self.is_fitted_ = False
         self.target_col_name = target_col_name
         self.feature_col_name = features_col_name
         self.prediction_col_name = prediction_col_name
-        self._mean_embeddings = None
+        self._centroids = None
 
-    def predict(self, emb, targets: list[str]) -> str:
+    @property
+    def centroids(self) -> dict[str, list[float]]:
+        return self._centroids
+
+    def _predict(self, emb) -> str:
         """
         It computes the cosine similarity between message and centroid of each class and predicts argmax
         :param emb: embeddings of the message
-        :param targets: classes to predict
         :return: class name
         """
         return max(
-            targets, key=lambda cr: sp.distance.cosine(emb, self._mean_embeddings[cr])
+            self._classes, key=lambda cr: sp.distance.cosine(emb, self._centroids[cr])
         )
 
-    def fit(self, x: pd.DataFrame, y: pd.Series = None):
+    def fit(self, x: pd.DataFrame, y: pd.Series) -> BaseEstimator:
         """
         Computes the centroid of each class
         :param x: training dataframe
         :param y: classes
         :return: self
         """
-        self._mean_embeddings = (
+        if y is None:
+            ValueError('requires y to be passed, but the target y is None')
+
+        self._classes = y.to_list()
+
+        self._centroids = (
             x.groupby(self.target_col_name)[self.feature_col_name]
             .mean(axis=1)
             .to_dict()
         )
+        self.is_fitted_ = True
+
         return self
 
-    def transform(self, x: pd.DataFrame, y: pd.Series = None):
+    def predict(self, x: pd.DataFrame):
         x_copy = x.copy()
-        y_list = y.to_list()
         x_copy[self.prediction_col_name] = x_copy[self.feature_col_name].swifter.apply(
-            lambda emb: self.predict(emb, y_list)
+            lambda emb: self._predict(emb)
         )
         return x_copy
-
-    def fit_transform(self, x: pd.DataFrame, y: pd.Series = None, **fit_params):
-        return self.fit(x, y).transform(x, y)
