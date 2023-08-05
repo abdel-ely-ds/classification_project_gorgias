ef606557ab750c46a894abccacf06d318f0f7e83
abdel-ely-ds <abdel.ely.ds@gmail.com>
2023-08-05 14:27:57 +0200

    encode message as mean of embds

--- src/gorgias_ml/constants.py
+++ src/gorgias_ml/constants.py
@@ -1,1 +1,2 @@
 EMBEDDINGS_COL_NAME = "email_sentence_embeddings"
+FEATURES = "features"
---
+++ src/gorgias_ml/encoders.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+import gorgias_ml.constants as cst
+
+
+class TicketMessageAverageEncoder(BaseEstimator, TransformerMixin):
+    """
+    Encode the message as the average of sentence embeddings.
+    It adds a new column features.
+    """
+
+    def __int__(self, col_name=cst.EMBEDDINGS_COL_NAME, features_name=cst.FEATURES):
+        self.col_name = col_name
+        self.features_name = features_name
+
+    @staticmethod
+    def _to_2d_array(emb: dict[str, list[float]]) -> np.array:
+        """
+        transforms the dict into a 2D array
+        :param emb: dict of sentence embeddings
+        :return: 2D array
+        """
+        return np.array(list(emb.values()))
+
+    def fit(self, x: pd.DataFrame = None, y: pd.Series = None):
+        return self
+
+    def transform(self, x: pd.DataFrame, y: pd.Series = None) -> pd.DataFrame:
+        x_copy = x.copy()
+        x_copy[self.features_name] = x_copy[self.col_name].swifter.apply(
+            lambda emb: np.mean(self._to_2d_array(emb), axis=0)
+        )
+
+        return x_copy
+
+    def fit_transform(self, x: pd.DataFrame, y: pd.Series = None, **fit_params):
+        return self.fit(x, y).transform(x, y)
